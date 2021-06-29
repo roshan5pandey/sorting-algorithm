3c72eb7e43cdb8ca0f50d9b3dab7e98694a9434a
roshan5pandey <37697478+roshan5pandey@users.noreply.github.com>
2021-06-28 21:26:46 -0700

    Add files via upload

---
+++ Roshan.java
@@ -0,0 +1,20 @@
+class Roshan
+{
+
+    public int add(int a,int b)
+    {
+        return(a+b);
+    }
+    public static void main(String[]args) {
+Roshan obj = new Roshan();
+
+int p=obj.add(6,7);
+
+
+        int a =54;
+        int b=98;
+        int c = a+b;
+        System.out.println("answer is   "+c+" "+p );
+    
+}
+}
